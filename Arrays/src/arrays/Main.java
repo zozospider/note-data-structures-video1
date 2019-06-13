42cd90e036938bc2043dc05848ca37820bd58836
zozospider <41692211+zozospider@users.noreply.github.com>
2019-06-13 21:42:47 +0800

    Initial commit

---
+++ Arrays/src/arrays/Main.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+    }
+}

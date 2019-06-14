369ec9f15ed5372d7e8c50e076df51f3f2a407e9
zozospider <41692211+zozospider@users.noreply.github.com>
2019-06-14 14:00:41 +0800

    update node-data-structures-video1

--- Arrays/src/arrays/Main.java
+++ Arrays/src/arrays/Main.java
@@ -4,10 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int [] arr = new int[10];
+        int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i;
         }
 
+        int[] scores = new int[]{100, 90, 60};
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+
+        scores[0] = 99;
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
     }
 }

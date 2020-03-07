ec7500ed9435f8c3b00e772d2822458bfb8a7bf0
zozospider <2642220896@qq.com>
2020-03-07 16:32:35 +0800

    update data structures

---
+++ 02-Arrays/06-Generic-Data-Structures/src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+}

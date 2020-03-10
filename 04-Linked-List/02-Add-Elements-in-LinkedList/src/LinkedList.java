3cf8129bc369bdbae6ef5cd101e7558c0be73cca
zozospider <41692211+zozospider@users.noreply.github.com>
2020-03-10 11:42:06 +0800

    update data structures

--- 04-Linked-List/02-Add-Elements-in-LinkedList/src/LinkedList.java
+++ 04-Linked-List/02-Add-Elements-in-LinkedList/src/LinkedList.java
@@ -3,22 +3,22 @@ public class LinkedList<E> {
     // 链表中的节点 (设置为 private 是为了屏蔽外部访问)
     private class Node {
 
-        // 元素 (设置为 public 是为了方便 LinkedList 直接访问)
-        public E e;
+        // 元素
+        E e;
 
-        // 下一个节点的引用 (设置为 public 是为了方便 LinkedList 直接访问)
-        public Node next;
+        // 下一个节点的引用
+        Node next;
 
-        public Node(E e, Node next) {
+        Node(E e, Node next) {
             this.e = e;
             this.next = next;
         }
 
-        public Node(E e) {
+        Node(E e) {
             this(e, null);
         }
 
-        public Node() {
+        Node() {
             this(null, null);
         }
 

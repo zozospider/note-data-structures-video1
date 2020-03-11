34b6bd7019181776967aa42ac08826b1a12e251d
zozospider <41692211+zozospider@users.noreply.github.com>
2020-03-11 11:27:27 +0800

    update data structures

--- 04-Linked-List/04-Query-and-Update-in-LinkedList/src/LinkedList.java
+++ 04-Linked-List/04-Query-and-Update-in-LinkedList/src/LinkedList.java
@@ -49,50 +49,6 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    // 在链表的 index(0-based) 位置添加新的元素 e
-    // 注: 因链表本身的结构特性不适合在随机位置插入, 此方法在链表中不是一个常用的操作, 练习用
-    public void add(int index, E e) {
-
-        // 如果要插入的索引位置小于 0, 或者大于当前 size 索引位置 (即所有元素最后位置之后), 则认为是非法操作, 抛出异常
-        if (index < 0 || index > size) {
-            throw new IllegalArgumentException("Add failed. Index is illegal.");
-        }
-
-        // 最终得到要插入的 index 节点的前一个节点 (从虚拟头节点开始算第一个)
-        // 也可以理解成用于记录某个索引的前一个节点 (从头部节点的前一个节点开始)
-        Node prev = dummyHead;
-
-        // 从虚拟头节点开始, 通过节点的 next 引用, 依次找下一个节点, 直到找到 index 前一个节点的引用: prev Node = (index - 1) Node = (index - 2).next Node
-        for (int i = 0; i < index; i++) {
-            prev = prev.next;
-        }
-
-        // 1. 创建数据为 e 的节点
-        // Node node = new Node(e);
-        // 2. 设置当前创建的节点的下一个节点的引用为 prev Node (index 前一个节点的引用) 的下一个节点的引用 prev.next Node
-        // node.next = prev.next;
-        // 3. 将 prev.next 的头部节点的引用设置为当前创建的节点, 即表示当前创建的节点为 prev Node 的下一个节点
-        // prev.next = node;
-
-        // 等效于上面 3 个步骤
-        // new Node(e, prev.next); 等效于步骤 1, 2
-        // prev.next = new Node(e, prev.next); 等效于步骤 3
-        prev.next = new Node(e, prev.next);
-
-        // 元素个数加 1
-        size++;
-    }
-
-    // 在链表的头部添加新的元素 e
-    public void addFirst(E e) {
-        add(0, e);
-    }
-
-    // 在链表的尾部添加新的元素 e
-    public void addLast(E e) {
-        add(size, e);
-    }
-
     // 获取链表的第 index(0-based) 个位置的元素
     // 注: 因链表本身的结构特性不适合在随机位置查询, 此方法在链表中不是一个常用的操作, 练习用
     public E get(int index) {
@@ -171,6 +127,50 @@ public class LinkedList<E> {
         */
 
         return false;
+    }
+
+    // 在链表的 index(0-based) 位置添加新的元素 e
+    // 注: 因链表本身的结构特性不适合在随机位置插入, 此方法在链表中不是一个常用的操作, 练习用
+    public void add(int index, E e) {
+
+        // 如果要插入的索引位置小于 0, 或者大于当前 size 索引位置 (即所有元素最后位置之后), 则认为是非法操作, 抛出异常
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Index is illegal.");
+        }
+
+        // 最终得到要插入的 index 节点的前一个节点 (从虚拟头节点开始算第一个)
+        // 也可以理解成用于记录某个索引的前一个节点 (从头部节点的前一个节点开始)
+        Node prev = dummyHead;
+
+        // 从虚拟头节点开始, 通过节点的 next 引用, 依次找下一个节点, 直到找到 index 前一个节点的引用: prev Node = (index - 1) Node = (index - 2).next Node
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        // 1. 创建数据为 e 的节点
+        // Node node = new Node(e);
+        // 2. 设置当前创建的节点的下一个节点的引用为 prev Node (index 前一个节点的引用) 的下一个节点的引用 prev.next Node
+        // node.next = prev.next;
+        // 3. 将 prev.next 的头部节点的引用设置为当前创建的节点, 即表示当前创建的节点为 prev Node 的下一个节点
+        // prev.next = node;
+
+        // 等效于上面 3 个步骤
+        // new Node(e, prev.next); 等效于步骤 1, 2
+        // prev.next = new Node(e, prev.next); 等效于步骤 3
+        prev.next = new Node(e, prev.next);
+
+        // 元素个数加 1
+        size++;
+    }
+
+    // 在链表的头部添加新的元素 e
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    // 在链表的尾部添加新的元素 e
+    public void addLast(E e) {
+        add(size, e);
     }
 
     @Override

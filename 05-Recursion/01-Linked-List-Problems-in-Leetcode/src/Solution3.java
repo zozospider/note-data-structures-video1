dd53d465c75a67cfd3207dcef70099c869b587e7
zozospider <41692211+zozospider@users.noreply.github.com>
2020-03-13 10:43:33 +0800

    update data structures

--- 05-Recursion/01-Linked-List-Problems-in-Leetcode/src/Solution3.java
+++ 05-Recursion/01-Linked-List-Problems-in-Leetcode/src/Solution3.java
@@ -9,8 +9,13 @@ public class Solution3 {
         dummyHead.next = head;
 
         ListNode prev = dummyHead;
+
         // 判断当前节点是否为 null, 如果为 null, 表示已遍历完所有节点
         while (prev.next != null) {
+
+            // 只有在当前操作节点的值不等于 val 的时候, 才需要将 prev 引用进行移动, 否则需要再次判断 prev 引用的下一个节点是否需要继续删除
+            // 如 [1 -> 3 -> 5]      链表中删除 3 的 prev 的引用变化为: prev(-1) [else 逻辑] -> prev(1) [if 逻辑执行删除] -> prev(5) [else 逻辑]
+            // 如 [1 -> 3 -> 3 -> 5] 链表中删除 3 的 prev 的引用变化为: prev(-1) [else 逻辑] -> prev(1) [if 逻辑执行删除] -> prev(1) [if 逻辑执行删除] -> prev(5) [else 逻辑]
 
             if (prev.next.val == val) {
                 // 如果当前节点的值等于 val, 需要将当前节点删除

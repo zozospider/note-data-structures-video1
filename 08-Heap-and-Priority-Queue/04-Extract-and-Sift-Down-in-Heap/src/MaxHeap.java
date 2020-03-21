96d29cf41dcbce56f94850e892085a660bab11c5
zozospider <2642220896@qq.com>
2020-03-21 20:45:55 +0800

    update data structures

--- 08-Heap-and-Priority-Queue/04-Extract-and-Sift-Down-in-Heap/src/MaxHeap.java
+++ 08-Heap-and-Priority-Queue/04-Extract-and-Sift-Down-in-Heap/src/MaxHeap.java
@@ -52,6 +52,65 @@ public class MaxHeap<E extends Comparable<E>> {
         }
     }
 
+    // 查询堆中的最大元素
+    public E findMax() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+        }
+        return array.get(0);
+    }
+
+    public E extractMax() {
+
+        E max = findMax();
+
+        // 将数组的第一个元素和最后一个元素交换, 并移除最后一个元素
+        array.swap(0, array.getSize() - 1);
+        array.removeLast();
+
+        // 下沉操作: 从数组的第一个 (完全二叉树的最上层的节点) 开始, 进行下沉操作, 直到满足堆结构的规则
+        siftDown(0);
+
+        return max;
+    }
+
+    // 下沉操作: 将 index 所在的节点不断下沉, 直到满足堆结构的规则 (即 index 所在的节点值大于等于 index 所在节点的孩子节点值)
+    private void siftDown(int index) {
+
+        // 如果 index 左孩子小于数组长度, 说明节点存在, 继续下沉循环
+        while (leftChildIndex(index) < array.getSize()) {
+
+            // 先求出 index 节点的左右孩子节点中更大值的孩子索引
+            int child = getLargerChildIndex(leftChildIndex(index), rightChildIndex(index));
+
+            if (array.get(index).compareTo(array.get(child)) >= 0) {
+
+                // 如果 index 节点值大于等于更大的孩子节点值, 则表示已经符合堆结构, 结束下沉循环
+                break;
+
+            } else {
+
+                // 否则说明 index 节点值小于更大的孩子节点值 (不符合堆结构)
+                // 交换 index 和其更大的孩子节点的元素, 并继续下沉循环
+
+                array.swap(index, child);
+                index = child;
+            }
+        }
+    }
+
+    // 获取左右孩子索引对应的节点中, 更大值的孩子索引
+    private int getLargerChildIndex(int left, int right) {
+
+        // 如果右孩子存在 (则左孩子一定存在), 且右孩子大于左孩子, 则返回右孩子
+        if (right < array.getSize()
+                && array.get(right).compareTo(array.get(left)) >= 0) {
+            return right;
+        }
+        // 否则返回左孩子
+        return left;
+    }
+
     // 返回完全二叉树的数组表示中 (从索引 0 开始表示第一个节点), 一个索引所表示的元素的父亲节点的索引
     private int parentIndex(int index) {
         if (index == 0) {

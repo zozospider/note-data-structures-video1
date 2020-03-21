eabc075be7b7f356671d638128b9bbcd3185bd69
zozospider <2642220896@qq.com>
2020-03-22 00:31:43 +0800

    update data structures

---
+++ 08-Heap-and-Priority-Queue/07-Priority-Queue-Problems-in-Leetcode/src/MaxHeap.java
@@ -0,0 +1,184 @@
+public class MaxHeap<E extends Comparable<E>> {
+
+    // 内部用动态数组存储数据
+    private Array<E> array;
+
+    public MaxHeap(int capacity) {
+        array = new Array<>(capacity);
+    }
+
+    public MaxHeap() {
+        array = new Array<>();
+    }
+
+    // 通过任意数组初始化堆 (Heapify 方式: 建议画图帮助理解执行逻辑)
+    // 时间复杂度 O(n) (证明方式较为复杂)
+    public MaxHeap(E[] arr) {
+
+        array = new Array<>(arr);
+
+        // 从完全二叉树的最后一个节点的父节点开始, 依次往前遍历, 直到第顶部节点
+        // 每次循环都对当前索引执行下沉操作
+        int startIndex = parentIndex(array.getSize() - 1);
+        for (int i = startIndex; i > 0; i--) {
+            siftDown(i);
+        }
+    }
+
+    // 获取堆中的元素个数
+    // 时间复杂度: O(1)
+    public int getSize() {
+        return array.getSize();
+    }
+
+    // 返回堆是否为空
+    // 时间复杂度: O(1)
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    // 查询堆中的最大元素
+    // 时间复杂度: O(1)
+    public E findMax() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Can not findMax when heap is empty.");
+        }
+        return array.get(0);
+    }
+
+    // 向堆中添加元素
+    // 时间复杂度: O(log n)
+    public void add(E e) {
+
+        // 先将元素 e 添加到数组的最后位置 (即添加到完全二叉树的最下层的最右边的叶子节点位置)
+        array.addLast(e);
+
+        // 上浮操作: 从数组的最后节 (完全二叉树的最下层的最右边的叶子节点) 开始, 进行上浮操作, 直到满足堆结构的规则
+        siftUp(array.getSize() - 1);
+    }
+
+    // 取出堆中最大元素
+    // 时间复杂度: O(log n)
+    public E extractMax() {
+
+        E max = findMax();
+
+        // 将数组的第一个元素和最后一个元素交换, 并移除最后一个元素
+        array.swap(0, array.getSize() - 1);
+        array.removeLast();
+
+        // 下沉操作: 从数组的第一个 (完全二叉树的最上层的节点) 开始, 进行下沉操作, 直到满足堆结构的规则
+        siftDown(0);
+
+        return max;
+    }
+
+    // 取出堆中的最大元素, 并且替换成元素 e (方式一: 先替换最大元素, 再执行下沉操作, 建议画图帮助理解执行逻辑)
+    // 时间复杂度: O(log n)
+    public E replace(E e) {
+
+        // 查询堆中的最大元素
+        // 时间复杂度: O(1)
+        E max = findMax();
+
+        // 将元素 e 设置成堆的最大值后, 执行下沉操作, 确保堆结构的正确性
+        array.set(0, e);
+        // 时间复杂度: O(log n)
+        siftDown(0);
+
+        return max;
+    }
+
+    // 取出堆中的最大元素, 并且替换成元素 e (方式二: 先取出最大元素, 再添加当前元素)
+    // 时间复杂度: 2 * O(log n)
+    public E replace2(E e) {
+
+        // 取出堆中的最大元素
+        // 时间复杂度: O(log n)
+        E max = extractMax();
+
+        // 将元素 e 添加到堆中
+        // 时间复杂度: O(log n)
+        add(e);
+
+        return max;
+    }
+
+    // 上浮操作: 将 index 所在的节点不断上浮, 直到满足堆结构的规则 (即 index 所在的节点值小于等于 index 所在节点的父节点值)
+    private void siftUp(int index) {
+
+        // 如果 index 大于 0, 说明节点存在, 继续上浮循环
+        while (index > 0) {
+
+            if (array.get(index).compareTo(array.get(parentIndex(index))) <= 0) {
+
+                // 如果 index 节点值小于等于父节点值, 则表示已经符合堆结构, 结束上浮循环
+                break;
+
+            } else {
+
+                // 否则说明 index 节点值大于 index 父节点值 (不符合堆结构)
+                // 交换 index 和其父节点的元素, 并继续上浮循环
+
+                array.swap(index, parentIndex(index));
+                index = parentIndex(index);
+            }
+        }
+    }
+
+    // 下沉操作: 将 index 所在的节点不断下沉, 直到满足堆结构的规则 (即 index 所在的节点值大于等于 index 所在节点的孩子节点值)
+    private void siftDown(int index) {
+
+        // 如果 index 左孩子小于数组长度, 说明节点存在, 继续下沉循环
+        while (leftChildIndex(index) < array.getSize()) {
+
+            // 先求出 index 节点的左右孩子节点中更大值的孩子索引
+            int child = getLargerChildIndex(leftChildIndex(index), rightChildIndex(index));
+
+            if (array.get(index).compareTo(array.get(child)) >= 0) {
+
+                // 如果 index 节点值大于等于更大的孩子节点值, 则表示已经符合堆结构, 结束下沉循环
+                break;
+
+            } else {
+
+                // 否则说明 index 节点值小于更大的孩子节点值 (不符合堆结构)
+                // 交换 index 和其更大的孩子节点的元素, 并继续下沉循环
+
+                array.swap(index, child);
+                index = child;
+            }
+        }
+    }
+
+    // 获取左右孩子索引对应的节点中, 更大值的孩子索引
+    private int getLargerChildIndex(int left, int right) {
+
+        // 如果右孩子存在 (则左孩子一定存在), 且右孩子大于左孩子, 则返回右孩子
+        if (right < array.getSize()
+                && array.get(right).compareTo(array.get(left)) >= 0) {
+            return right;
+        }
+        // 否则返回左孩子
+        return left;
+    }
+
+    // 返回完全二叉树的数组表示中 (从索引 0 开始表示第一个节点), 一个索引所表示的元素的父亲节点的索引
+    private int parentIndex(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("index-0 does not have parent.");
+        }
+        return (index - 1) / 2;
+    }
+
+    // 返回完全二叉树的数组表示中 (从索引 0 开始表示第一个节点), 一个索引所表示的元素的左孩子节点的索引
+    private int leftChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    // 返回完全二叉树的数组表示中 (从索引 0 开始表示第一个节点), 一个索引所表示的元素的右孩子节点的索引
+    private int rightChildIndex(int index) {
+        return index * 2 + 2;
+    }
+
+}
---
+++ 08-Heap-and-Priority-Queue/07-Priority-Queue-Problems-in-Leetcode/src/Solution.java
@@ -0,0 +1,85 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+
+    // 可比较的元素对象, 存储元素值和其出现的频次 (按照出现的频次从小到大排序)
+    private class Element implements Comparable<Element> {
+
+        // 元素值
+        int element;
+        // 出现的频次
+        int frequency;
+
+        public Element(int element, int frequency) {
+            this.element = element;
+            this.frequency = frequency;
+        }
+
+        // 按照出现的频次 frequency 倒序排列 (频次最小的元素优先级最高)
+        @Override
+        public int compareTo(Element element) {
+            if (this.frequency > element.frequency) {
+                return -1;
+            } else if (this.frequency < element.frequency) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        // 遍历 nums, 将 num (key) 和其频次 (value) 存储到 map 映射中
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+
+        // 遍历 map, 将 map 中的元素和其出现的频次加入到优先队列中
+        Queue<Element> priorityQueue = new MaxHeapPriorityQueue<>();
+
+        // 遍历 map 所有数据, 加入到优先队列中 (队列中存储 k 个元素)
+        for (int key : map.keySet()) {
+
+            // 获取当前频次
+            int frequency = map.get(key);
+
+            if (priorityQueue.getSize() < k) {
+
+                // 如果优先队列中还不足 k 个元素, 则填满优先队列
+                priorityQueue.enqueue(new Element(key, frequency));
+
+            } else {
+
+                // 否则, 说明优先队列已经有 k 个元素了, 此时需要将当前元素的频次与优先队列中的最小频次进行比较
+
+                // 如果当前元素的频次大于优先队列中的最小频次, 则需要将优先队列中的最小频次元素替换成当前元素
+
+                if (frequency > priorityQueue.getFront().frequency) {
+
+                    // 将优先队列中的最小频次元素出队
+                    priorityQueue.dequeue();
+                    // 将当前元素入队
+                    priorityQueue.enqueue(new Element(key, frequency));
+                }
+            }
+        }
+
+        // 此时优先队列中的所有 key 即为 nums 数组中出现频率最高的 k 个数
+        // 转换成 list 返回
+        List<Integer> keyList = new LinkedList<>();
+        while (!priorityQueue.isEmpty()) {
+            keyList.add(priorityQueue.dequeue().element);
+        }
+        return keyList;
+    }
+
+}

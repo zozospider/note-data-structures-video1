e2b45cc1894e743bb9c80d21f8503066bbfa6735
zozospider <2642220896@qq.com>
2020-03-24 21:01:40 +0800

    update data structures

---
+++ 09-Segment-Tree/04-Query-in-Segment-Tree/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Integer[] numbers = {-2, 0, 3, -5, 2, -1};
+        SegmentTree<Integer> segmentTree = new SegmentTree<>(numbers, (e1, e2) -> e1 + e2);
+        System.out.println(segmentTree);
+
+        System.out.println(segmentTree.query(0, 2));
+        System.out.println(segmentTree.query(0, 5));
+        System.out.println(segmentTree.query(2, 5));
+    }
+
+}
